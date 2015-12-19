fada5619f62f537a090429e7625c9ce94fab23b2
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-12-20 00:44:48 +0900

    アンブレラヘッダが変だったので修正

--- HOGEFuga/HOGEFuga.h
+++ HOGEFuga/HOGEFuga.h
@@ -1,10 +1,3 @@
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for ActionExtensionNoUIEmbeddedLib.
-FOUNDATION_EXPORT double ActionExtensionNoUIEmbeddedLibVersionNumber;
-
-//! Project version string for ActionExtensionNoUIEmbeddedLib.
-FOUNDATION_EXPORT const unsigned char ActionExtensionNoUIEmbeddedLibVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ActionExtensionNoUIEmbeddedLib/PublicHeader.h>
 #import <HOGEFuga/HOGEFugaService.h>
