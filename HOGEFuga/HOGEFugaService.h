58049e5a97bf3150927d2027d8e3cc9a8682dd31
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-12-20 00:00:05 +0900

    ベース作成

---
+++ HOGEFuga/HOGEFugaService.h
@@ -0,0 +1,15 @@
+//
+//  HOGEFugaService.h
+//  HOGEFuga
+//
+//  Created by 清 貴幸 on 2015/12/19.
+//  Copyright © 2015年 Hoge, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HOGEFugaService : NSObject
+
++ (void)show;
+
+@end
---
+++ HOGEFuga/HOGEFugaService.m
@@ -0,0 +1,17 @@
+//
+//  HOGEFugaService.m
+//  HOGEFuga
+//
+//  Created by 清 貴幸 on 2015/12/19.
+//  Copyright © 2015年 Hoge, inc. All rights reserved.
+//
+
+#import "HOGEFugaService.h"
+
+@implementation HOGEFugaService
+
++ (void)show {
+    NSLog(@"hogehoge");
+}
+
+@end
