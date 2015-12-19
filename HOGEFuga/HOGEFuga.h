513fd8da7795e3d88160fca043fd04a3dc139d2c
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-12-19 23:52:41 +0900

    Initial Commit

---
+++ HOGEFuga/HOGEFuga.h
@@ -0,0 +1,13 @@
+//
+//  HOGEFuga.h
+//  HOGEFuga
+//
+//  Created by 清 貴幸 on 2015/12/19.
+//  Copyright © 2015年 Hoge, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HOGEFuga : NSObject
+
+@end
---
+++ HOGEFuga/HOGEFuga.m
@@ -0,0 +1,13 @@
+//
+//  HOGEFuga.m
+//  HOGEFuga
+//
+//  Created by 清 貴幸 on 2015/12/19.
+//  Copyright © 2015年 Hoge, inc. All rights reserved.
+//
+
+#import "HOGEFuga.h"
+
+@implementation HOGEFuga
+
+@end
