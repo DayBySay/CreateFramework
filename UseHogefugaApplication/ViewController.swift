a643ccb49235ed1691aa50fed8bcb263da3297eb
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-02-22 01:39:13 +0900

    サンプル実装追加

--- UseHogefugaApplication/ViewController.swift
+++ UseHogefugaApplication/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         HOGEFugaService.show()
+        NSLog("%@", HOGEFugaService.ADID())
     }
 
     override func didReceiveMemoryWarning() {
